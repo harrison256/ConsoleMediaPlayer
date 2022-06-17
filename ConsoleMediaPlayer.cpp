0f7b2fa7a8eb4a163bebcb442f097e40fd665bc2
harri <harri@HARRISON-PC>
2022-06-17 11:02:30 +0100

    Add project files.

---
+++ ConsoleMediaPlayer.cpp
@@ -0,0 +1,12 @@
+﻿// ConsoleMediaPlayer.cpp : Defines the entry point for the application.
+//
+
+#include "ConsoleMediaPlayer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
